cad12aa5ed8135557088c9383c5c02096d241139
chakina <104717105+chakina@users.noreply.github.com>
2023-10-11 17:55:17 +0530

    Add files via upload

---
+++ steps.js
@@ -0,0 +1,67 @@
+console.log("hello anyone");
+
+let var1 = 3;
+
+let var2 = 6;
+
+console.log(var2);
+
+let age_of_a_person = 76;
+
+let ageOfAPerson = 63;
+
+console.log(ageOfAPerson);
+
+const drivingLicense = 18;
+
+console.log(drivingLicense);
+
+let number1 = 22;
+let number2 = 44;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+console.log(number1 ** number2);
+
+
+let age = 12;
+age++;
+
+let apple = 67;
+apple--;
+
+console.log(age);
+console.log(apple);
+
+let rays = 98;
+
+rays += 7;
+console.log(rays);
+rays -= 8;
+console.log(rays);
+rays *= 9;
+console.log(rays);
+rays /= 5;
+console.log(rays);
+rays %= 6;
+console.log(rays);
+
+let number11 = 44;
+let number22 = 88;
+
+console.log(number11==number22);
+console.log(number11!=number22);
+console.log(number11===number22);
+console.log(number11<number22);
+console.log(number11>number22);
+console.log(number11<=number22);
+console.log(number11>=number22);
+
+let number6 = 99;
+let number7 = "99";
+
+console.log(number6==number7);
+console.log(number6===number7);
